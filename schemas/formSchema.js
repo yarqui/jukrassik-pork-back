f1d78bda53002bda5a08e0a1815f1bf72c9cfbc7
yarqui <108986486+yarqui@users.noreply.github.com>
2023-07-09 17:47:07 +0300

    schema change

--- schemas/formSchema.js
+++ schemas/formSchema.js
@@ -4,7 +4,7 @@ const EMAIL_REGEX =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/; // eslint-disable-line
 
 const formSchema = Joi.object({
-  name: Joi.string().min(2).max(32),
+  name: Joi.string().min(2).max(32).allow(""),
   email: Joi.string().pattern(EMAIL_REGEX).required(),
   message: Joi.string().min(4).required(),
 }).unknown(true);
